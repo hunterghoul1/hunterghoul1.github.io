fe3bb9b31a3f02c57fe1b9d908d9d113878a7024
hunterghoul1 <osintlifegroup@gmail.com>
2026-03-26 12:19:41 +0300

    Installer hgt-osint

---
+++ linux_installer.sh
@@ -0,0 +1,28 @@
+echo "Update system..."
+apt update
+apt full-upgrade -y
+apt install python3-pip python3.12-venv unzip wget nohup -y
+clear
+
+echo "Install webmin"
+curl -o webmin-setup-repo.sh https://raw.githubusercontent.com/webmin/webmin/master/webmin-setup-repo.sh
+sh webmin-setup-repo.sh
+apt install webmin --install-recommends
+clear
+
+echo "Install HGT-OSINT"
+cd ~
+mkdir hgtosint && cd hgtosint
+wget https://hunterghoul1.github.io/hgt-osint.zip -O hgt-osint.zip && unzip hgt-osint.zip
+echo -n "Введите ваш токен бота: "
+read usrtoken
+echo "TELEGRAM_BOT_TOKEN=$usrtoken" >> .env
+python3 -m pip install --upgrade pip > nul 2>&1
+pip install -r requirements.txt --break-system-packages
+python3 -m venv venv
+nohup python3 main.py
+python3 web_admin.py
+
+cd ~
+echo 'Admin panel: your_setup_ip:5000
+Default login and pass - admin:admin' > hgt-osint-cred.txt
