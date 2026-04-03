a289629309a834f1181cb63182baf7a386160702
hunterghoul1 <osintlifegroup@gmail.com>
2026-04-03 13:28:14 +0300

    Fix wget option from '-o' to '-O' for output

--- tirreno.sh
+++ tirreno.sh
@@ -2,7 +2,7 @@ apt update
 apt install wget -y
 clear
 echo 'Install docker'
-wget https://hunterghoul1.github.io/dockerinstaller.sh -o dockerinstaller.sh && ./dockerinstaller.sh && rm dockerinstaller.sh
+wget https://hunterghoul1.github.io/dockerinstaller.sh -O dockerinstaller.sh && ./dockerinstaller.sh && rm dockerinstaller.sh
 clear
 echo 'Install tirreno'
 curl -sL tirreno.com/t.yml | docker compose -f - up -d
