74060f8ba754da81062817f480b3f813304ac3f1
hunterghoul1 <osintlifegroup@gmail.com>
2026-04-03 13:49:11 +0300

    Create docker-compose.yml for Tirreno app setup
    Added a Docker Compose configuration for the Tirreno app with PostgreSQL support.

--- tirreno.sh
+++ tirreno.sh
@@ -5,4 +5,49 @@ echo 'Install docker'
 wget https://hunterghoul1.github.io/dockerinstaller.sh -O dockerinstaller.sh && chmod +x dockerinstaller.sh && ./dockerinstaller.sh && rm dockerinstaller.sh
 clear
 echo 'Install tirreno'
-curl -sL tirreno.com/t.yml | docker compose -f - up -d
+echo -n 'ip server: '
+read usrip
+echo -n 'port tirreno: '
+read usrport
+echo '# tirreno with PostgreSQL
+#
+# Signup via http://localhost:8585/signup
+
+services:
+  tirreno-app:
+    image: tirreno/tirreno:latest
+    pull_policy: always
+    ports:
+      - "$usrport:80"
+    environment:
+      DATABASE_URL: "postgres://tirreno:secret@tirreno-db:5432/tirreno"
+      SITE: "$usrip:8585"
+      FORCE_HTTPS: "false"
+    volumes:
+      - tirreno-config:/var/www/html/config/local
+      - tirreno-logs:/var/www/html/assets/logs
+    networks:
+      - tirreno-network
+    depends_on:
+      - tirreno-db
+
+  tirreno-db:
+    image: postgres:15
+    environment:
+      POSTGRES_DB: tirreno
+      POSTGRES_USER: tirreno
+      POSTGRES_PASSWORD: secret
+    volumes:
+      - tirreno-db:/var/lib/postgresql/data
+    networks:
+      - tirreno-network
+
+networks:
+  tirreno-network:
+
+volumes:
+  tirreno-config:
+  tirreno-logs:
+  tirreno-db:' > docker-compose.yml
+
+docker compose up -d
