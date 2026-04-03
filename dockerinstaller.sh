3ef79b59a79a3d5bcbe6593d52248bd4402f75e4
hunterghoul1 <osintlifegroup@gmail.com>
2026-04-03 13:14:52 +0300

    Add dockerinstaller.sh for Docker installation
    Install Docker and Docker Compose with a script.

---
+++ dockerinstaller.sh
@@ -0,0 +1,10 @@
+apt update
+apt install wget -y
+apt remove $(dpkg --get-selections docker.io docker-compose docker-compose-v2 docker-doc podman-docker containerd runc | cut -f1)
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh ./get-docker.sh --dry-run
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v5.0.1/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+docker compose version
