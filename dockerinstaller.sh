cddc0d61ecc50ae1f3afb3928b45d39636dccdf9
hunterghoul1 <osintlifegroup@gmail.com>
2026-05-18 13:33:09 +0300

    Update dockerinstaller.sh

--- dockerinstaller.sh
+++ dockerinstaller.sh
@@ -1,8 +1,9 @@
 apt update
 apt install wget -y
 apt remove $(dpkg --get-selections docker.io docker-compose docker-compose-v2 docker-doc podman-docker containerd runc | cut -f1)
-curl -fsSL https://get.docker.com -o get-docker.sh
+curl -fsSL https://get.docker.com -O get-docker.sh
 sh ./get-docker.sh
+rm get-docker.sh
 DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
 mkdir -p $DOCKER_CONFIG/cli-plugins
 curl -SL https://github.com/docker/compose/releases/download/v5.0.1/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
